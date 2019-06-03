69977f9d767d6d93787c6d25f990a503c191bc4f
Alex Curci <alex.curci@gmail.com.com>
2019-06-03 23:15:54 +0000

    Added forEach loop to retreive avatar url

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var credentials = require('./secrets.js');
-let github_token = credentials.GITHUB_TOKEN;
+let githubToken = credentials.GITHUB_TOKEN;
 console.log('cred', credentials.GITHUB_TOKEN);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -10,18 +10,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + 
       "/contributors",
     headers: {
-      'User-Agent': 'request'
-    },
-    Authorization: github_token 
+      'User-Agent': 'request',
+      'Authorization': githubToken 
+    }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    console.log(body);
+    if (!err && res.statusCode == 200) {
+      let data = JSON.parse(body);
+      cb(err, data);
+    } else {
+      cb(err, body);
+    }
   }); 
 
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+getRepoContributors("lighthouse-labs", "laser_shark", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+  result.forEach((element) => {
+    console.log(element.avatar_url);
+  });
 });
