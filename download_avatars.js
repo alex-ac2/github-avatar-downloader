7d37a5a52c55853d61703d900d9492c87318aa0d
Alex Curci <alex.curci@gmail.com.com>
2019-06-03 22:27:56 +0000

    Format github token in private module

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,24 @@
 var request = require('request');
+var credentials = require('./secrets.js');
+let github_token = credentials.GITHUB_TOKEN;
+console.log('cred', credentials.GITHUB_TOKEN);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + 
+      "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    },
+    Authorization: github_token 
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  }); 
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
