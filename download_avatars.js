42a6f5f4f51be3034ca9312a01c1a868867ddc31
Alex Curci <alex.curci@gmail.com.com>
2019-06-04 03:24:21 +0000

    Added various error state messages to handle repo not found and missing arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,17 @@
+// Obtain command line arguments for repo owner and repo name
+var terminalArgs = process.argv.splice(2);
+
 var request = require('request');
 var credentials = require('./secrets.js');
 var fs = require('fs');
 
-let githubToken = credentials.GITHUB_TOKEN;
-
-
+// Github token 
+var githubToken = credentials.GITHUB_TOKEN;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+
+function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
     url:  "https://api.github.com/repos/" + repoOwner + "/" + repoName + 
       "/contributors",
@@ -18,17 +21,20 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  // GET Request to pass parsed response data to callback function
   request.get(options, function(err, res, body) {
-    // console.log(body);
+    let data = JSON.parse(body);
+
     if (!err && res.statusCode == 200) {
-      let data = JSON.parse(body);
-      cb(err, data);
-    } else {
-      cb(err, body);
+      callback(err, data);
+    } else if (data.message.toLowerCase() == 'not found') {
+      console.log("Repo owner or repo name not found, please check your spelling");
+      callback(true, data);
     }
   }); 
 }
 
+// Second GET request to retreive avatar image
 function downloadImageByURL(url, filePath) {
   request.get(url, filePath) 
     .on('error', function(err) {
@@ -40,12 +46,20 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath));
 }
 
-
-getRepoContributors("lighthouse-labs", "laser_shark", function(err, result) {
-  console.log("Errors:", err);
-  result.forEach((element) => {
-    console.log(element.avatar_url);
-    downloadImageByURL(element.avatar_url, 
-      './avatar/' + element.login + '.png');
+// Check repo owner and repo name
+if (terminalArgs.length !== 2) {
+  console.log("Please provide only two names, <repo owner> & <repo name>");
+} else {
+  //Function Call to initate avatar lookup
+  getRepoContributors(terminalArgs[0], terminalArgs[1], function(err, result) {
+    if (err) {
+      console.log("Error ocurred, please resubmit.\n");
+    } else {
+      result.forEach((element) => {
+        console.log(element.avatar_url);
+        downloadImageByURL(element.avatar_url, 
+          './avatar/' + element.login + '.png');
+      });
+    }
   });
-});
+}
